0a824955fdbf78526794d21e45b69dad4c0bd4d2
zvoronz <voron.zvs@gmail.com>
2017-09-18 21:45:06 +0000

    + intial commit

---
+++ photobooth.py
@@ -0,0 +1,25 @@
+import pygame
+
+pygame.init()
+pygame.mouse.set_visible(0)
+screen = pygame.display.set_mode((640, 480), pygame.HWSURFACE | pygame.FULLSCREEN, 32)
+
+done = False
+clock = pygame.time.Clock()
+
+BLUE = (0, 0, 255)
+YELLOW = (0, 255, 255)
+
+font = pygame.font.Font(None, 75)
+text = font.render('Photobooth', True, YELLOW)
+
+while done == False:
+	for event in pygame.event.get():
+		if event.type == pygame.MOUSEBUTTONUP:
+			done = True
+	
+	screen.fill(BLUE)
+	screen.blit(text, (180, 50))
+
+	pygame.display.flip()
+	clock.tick(60)
