72c6ef0884e71fd77159a547efa28d5660a82554
zvoronz <voron.zvs@gmail.com>
2017-09-21 00:23:41 +0300

    + added screen system + added virtualKeyboard module

--- photobooth.py
+++ photobooth.py
@@ -1,26 +1,59 @@
 import pygame
+from virtualKeyboard import VirtualKeyboard
+import screen
+
+WELCOME_SCREEN = [{
+"background":[139,174,233],
+"labels":[
+	{
+		"position":[20, 100],
+		"text":"Welcome to Snappy Campers",
+		"color":[0, 0, 0],
+		"font":"Arial",
+		"font_size":85
+	},
+	{
+		"position":[20, 350],
+		"text":"Please Touch Screen to Start",
+		"color":[0, 0, 0],
+		"font":"Arial",
+		"font_size":85
+	}]
+},
+{
+"background":[139,174,233],
+"labels":[
+	{
+		"position":[400, 50],
+		"text":"5",
+		"color":[0, 0, 0],
+		"font":"Arial",
+		"font_size":385
+	}]
+}]
 
 pygame.init()
-pygame.mouse.set_visible(0)
-screen = pygame.display.set_mode((640, 480), pygame.HWSURFACE, 32)
+#pygame.mouse.set_visible(0)
+welcome = screen.Screen(WELCOME_SCREEN[1])
+s5 = 0;
+screen = pygame.display.set_mode((1024, 600), pygame.HWSURFACE, 32)
 
 done = False
 clock = pygame.time.Clock()
 
-BLUE = (0, 0, 255)
-YELLOW = (0, 255, 255)
-
-font = pygame.font.Font(None, 75)
-text = font.render('Photobooth', True, YELLOW)
+#vkey = VirtualKeyboard(screen)
+#input_text = vkey.run()
 
 while done == False:
 	for event in pygame.event.get():
-		if event.type == pygame.MOUSEBUTTONUP:
-			done = True
+		if event.type == pygame.KEYUP:
+			if event.key == pygame.K_ESCAPE:
+				done = True
 
-	screen.fill(BLUE)
-	screen.blit(text, (180, 50))
+	welcome.render(screen)
 
+	#if input_text == 'quit':
+	#	done = True
 	pygame.display.flip()
 	clock.tick(60)
 pygame.quit()
---
+++ screen.py
@@ -0,0 +1,51 @@
+﻿#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      VoRoN
+#
+# Created:     20.09.2017
+# Copyright:   (c) VoRoN 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+import pygame
+
+class Screen:
+	def __init__(self, config):
+		self.fontCache = FontCache()
+		self.background = tuple(config['background'])
+		self.labels = []
+		for label in config['labels']:
+			font = self.fontCache.getFont(label['font'], label['font_size'])
+			if font == None:
+				font = pygame.font.SysFont(label['font'], label['font_size'], True)
+				self.fontCache.addFont(label['font'], label['font_size'], font)
+			self.labels.append(Label(font.render(label['text'], True, tuple(label['color'])),
+									tuple(label['position'])))
+	def render(self, screen):
+		screen.fill(self.background)
+		for label in self.labels:
+		    label.render(screen)
+
+class Label:
+	def __init__(self, text, position):
+		self.text = text
+		self.position = position
+	def render(self, screen):
+		position = (screen.get_width() / 2 - self.text.get_width() / 2,
+					screen.get_height() / 2 - self.text.get_height() / 2)
+		screen.blit(self.text, position)
+
+class FontCache:
+	def __init__(self):
+		self.fonts = []
+
+	def getFont(self, name, size):
+		for font in self.fonts:
+			if font['name'] == name and font['size'] == size:
+				return font['font']
+			else:
+				return None
+
+	def addFont(self, name, size, font):
+		self.fonts.append({'name':name, 'size':size, 'font':font})
